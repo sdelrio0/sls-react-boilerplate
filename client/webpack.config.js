b2bbc64eff1b94055ec9ff83829cae0c46c1f77c
Sergio del Rio <sdelrio0@gmail.com>
2016-05-17 09:18:57 -0500

    Adds a decent webpack config

---
+++ client/webpack.config.js
@@ -0,0 +1,101 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const validate = require('webpack-validator');
+const parts = require('./webpack.config.parts');
+const pkg = require('./package.json');
+
+const TARGET = process.env.npm_lifecycle_event;
+
+const PATHS = {
+  app:   path.join(__dirname, 'app'),
+  style: [
+    path.join(__dirname, 'node_modules', 'purecss'),
+    path.join(__dirname, 'app', 'main.css')
+  ],
+  build: path.join(__dirname, 'dist')
+};
+
+const common = {
+  // Entry accepts a path or an object of entries.
+  // We'll be using the latter form given it's
+  // convenient with more complex configurations.
+  entry: {
+    style: PATHS.style,
+    app: PATHS.app
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: PATHS.build,
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        // Enable caching for improved performance during development
+        // It uses default OS directory by default. If you need something
+        // more custom, pass a path to it. I.e., babel?cacheDirectory=<path>
+        loaders: ['babel?cacheDirectory'],
+        // Parse only app files! Without this it will go through entire project.
+        // In addition to being slow, that will most likely result in an error.
+        include: PATHS.app
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack demo'
+    })
+  ]
+};
+
+var config;
+
+switch(TARGET) {
+  case 'stats':
+  case 'build':
+    config = merge(
+      common,
+      {
+        devtool: 'source-map',
+        output: {
+          path: PATHS.build,
+          filename: '[name].[chunkhash].js',
+          // This is used for require.ensure. The setup
+          // will work without but this is useful to set.
+          chunkFilename: '[chunkhash].js'
+        }
+      },
+      parts.clean(path.join(PATHS.build, '*')),
+      parts.setFreeVariable(
+        'process.env.NODE_ENV',
+        'production'
+      ),
+      parts.extractBundle({
+        name: 'vendor',
+        entries: Object.keys(pkg.dependencies) // ***
+      }),
+      parts.extractCSS(PATHS.style),
+      parts.purifyCSS([PATHS.app]),
+      parts.minify()
+    );
+    break;
+
+  default:
+    config = merge(
+      common,
+      {
+        devtool: 'source-map'
+      },
+      parts.setupCSS(PATHS.style),
+      parts.devServer({
+        host: process.env.HOST,
+        port: process.env.PORT
+      })
+    );
+}
+
+module.exports = validate(config);
---
+++ client/webpack.config.parts.js
@@ -0,0 +1,163 @@
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const PurifyCSSPlugin = require('purifycss-webpack-plugin');
+
+// DEV Server with Host Module Replacement
+exports.devServer = function(options) {
+  return {
+    devServer: {
+      // Enable history API fallback so HTML5 History API based
+      // routing works. This is a good default that will come
+      // in handy in more complicated setups.
+      historyApiFallback: true,
+
+      // Unlike the cli flag, this doesn't set
+      // HotModuleReplacementPlugin!
+      hot: true,
+      inline: true,
+      //progress: true,
+
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      // Parse host and port from env to allow customization.
+      //
+      // If you use Vagrant or Cloud9, set
+      // host: options.host || '0.0.0.0';
+      //
+      // 0.0.0.0 is available to all network devices
+      // unlike default `localhost`.
+      host: options.host,
+      port: options.port
+    },
+
+    plugins: [
+      // Enable multi-pass compilation for enhanced performance
+      // in larger projects. Good default.
+      new webpack.HotModuleReplacementPlugin({
+        multiStep: true
+      })
+    ]
+  };
+};
+
+// CSS Loaders
+exports.setupCSS = function(paths) {
+  return {
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loaders: ['style', 'css'],
+          include: paths
+        }
+      ]
+    }
+  }
+};
+
+// Minify with UglifyJS
+exports.minify = function() {
+  return {
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      //  mangle: {
+      //    // Mangle matching properties
+      //    props: /matching_props/,
+      //    // Don't mangle these
+      //    except: [
+      //      'Array', 'BigInteger', 'Boolean', 'Buffer'
+      //    ]
+      //  }
+      })
+    ]
+  };
+};
+
+// Minification process will eliminate the branches that evaluate as false.
+// They won't contribute towards the size of the bundle as a result.
+exports.setFreeVariable = function(key, value) {
+  const env = {};
+
+  env[key] = JSON.stringify(value);
+
+  return {
+    plugins: [
+      new webpack.DefinePlugin(env)
+    ]
+  };
+};
+
+// Creates a manifest.js file and splits the bundle into chunks
+exports.extractBundle = function(options) {
+  const entry = {};
+
+  entry[options.name] = options.entries;
+
+  return {
+    // Define an entry point needed for splitting.
+    entry: entry,
+    plugins: [
+      // Extract bundle and manifest files. Manifest is
+      // needed for reliable caching.
+      new webpack.optimize.CommonsChunkPlugin({
+        names: [options.name, 'manifest'],
+
+        // options.name modules only
+        minChunks: Infinity
+      })
+    ]
+  };
+};
+
+// Cleans up the build directory before each build
+exports.clean = function(path) {
+  return {
+    plugins: [
+      new CleanWebpackPlugin([path], {
+        // Without `root` CleanWebpackPlugin won't point to our
+        // project and will fail to work.
+        root: process.cwd()
+      })
+    ]
+  };
+};
+
+// Extracts CSS to another file
+exports.extractCSS = function(paths) {
+  return {
+    module: {
+      loaders: [
+        // Extract CSS during build
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract('style', 'css'),
+          include: paths
+        }
+      ]
+    },
+    plugins: [
+      // Output extracted CSS to a file
+      new ExtractTextPlugin('[name].[chunkhash].css')
+    ]
+  };
+};
+
+// Purify CSS to minimize size
+exports.purifyCSS = function(paths) {
+  return {
+    plugins: [
+      new PurifyCSSPlugin({
+        basePath: process.cwd(),
+        // `paths` is used to point PurifyCSS to files not
+        // visible to Webpack. You can pass glob patterns
+        // to it.
+        paths: paths
+      })
+    ]
+  };
+};
